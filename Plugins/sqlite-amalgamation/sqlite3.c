7a3ab6c1ec59bc1bd912eae90791658569994f93
gilzoide <gilzoide@gmail.com>
2025-02-15 09:18:04 -0300

    Set compile definitions in a header file for IL2CPP in all platforms

--- Plugins/sqlite-amalgamation/sqlite3.c
+++ Plugins/sqlite-amalgamation/sqlite3.c
@@ -1,3 +1,5 @@
+#include "sqlite3_defines.h"
+
 /******************************************************************************
 ** This file is an amalgamation of many separate C source files from SQLite
 ** version 3.48.0.  By combining all the individual C code files into this
